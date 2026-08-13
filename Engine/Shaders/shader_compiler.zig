44a360f364afe5af7d458aa4c19fa53fdd28828e
frogle-dev <jonah0602@outlook.com>
2026-08-13 08:42:18 -0700

    spawn slangc in shader_compiler.zig

--- Engine/Shaders/shader_compiler.zig
+++ Engine/Shaders/shader_compiler.zig
@@ -14,10 +14,22 @@ pub fn main(init: std.process.Init) !void {
     while (try shader_source_iter.next(init.io)) |shader_source| {
         if (shader_source.kind != .file) continue;
 
+        if (!std.mem.eql(u8, std.Io.Dir.path.extension(shader_source.name), ".slang")) continue;
+
         const shader_buf = try shader_source_dir.readFileAlloc(init.io, shader_source.name, init.arena.allocator(), .unlimited);
         _ = shader_buf;
 
         // compile shader to spirv
+        const shader_path = try std.Io.Dir.path.join(init.arena.allocator(), &.{ shader_source_path, shader_source.name });
+
+        const slangc_args = [_][]const u8{ "slangc", shader_path, "-target", "spirv", "-profile", "spirv_1_4", "-emit-spirv-directly", "-fvk-use-entrypoint-name", "-entry", "main" };
+
+        const result = try std.process.run(init.arena.allocator(), init.io, .{
+            .argv = &slangc_args,
+        });
+
+        std.debug.print("slangc stderr: {s}", .{result.stderr});
+        std.debug.print("slangc stdout: {s}", .{result.stdout});
 
         // write compiled binary to compiled_shaders_dir
     }
