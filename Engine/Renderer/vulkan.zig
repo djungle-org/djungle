98540f95b2f649e28448000900c393be340c2109
frogle-dev <jonah0602@outlook.com>
2026-09-23 18:12:27 -0700

    vulkan physical device picking

--- Engine/Renderer/vulkan.zig
+++ Engine/Renderer/vulkan.zig
@@ -25,9 +25,14 @@ pub const Vulkan = struct {
         ExtensionNotSupported,
         FailedToGetInstanceProcAddr,
         SdlVulkanCreateSurfaceFailed,
+        FailedToFindSupportedGPU,
     };
 
-    pub const api_version = vk.API_VERSION_1_4;
+    pub const api_version = vk.API_VERSION_1_3;
+
+    pub const required_extensions = [_][:0]const u8{
+        vk.extensions.khr_swapchain.name,
+    };
 
     allocator: std.mem.Allocator,
 
@@ -42,8 +47,10 @@ pub const Vulkan = struct {
 
     surface: vk.SurfaceKHR,
 
+    physical_device: vk.PhysicalDevice,
+
     /// PropertyType must be vk.LayerProperties or vk.ExtensionProperties
-    fn allSupported(required: []const [*:0]const u8, comptime PropertyType: type, properties: []const PropertyType) !bool {
+    fn allSupported(required: []const [:0]const u8, comptime PropertyType: type, properties: []const PropertyType) !bool {
         for (required) |req| {
             var found = false;
             for (properties) |prop| {
@@ -57,7 +64,7 @@ pub const Vulkan = struct {
                     }
                 };
 
-                if (std.mem.eql(u8, std.mem.span(req), std.mem.sliceTo(&available, 0))) {
+                if (std.mem.eql(u8, req, std.mem.sliceTo(&available, 0))) {
                     found = true;
                     break;
                 }
@@ -71,18 +78,18 @@ pub const Vulkan = struct {
         return true;
     }
 
-    /// c_array must have c_array_len valid elements
-    fn cArrayToArrayList(gpa: std.mem.Allocator, comptime CType: type, comptime ZType: type, c_array: [*c]const CType, c_array_len: u32) !std.ArrayList(ZType) {
-        var array_list = try std.ArrayList(ZType).initCapacity(gpa, c_array_len);
-        array_list.items.len = c_array_len;
-
-        var i: u32 = 0;
-        while (i < c_array_len) : (i += 1) {
-            array_list.items[i] = @ptrCast(c_array[i]);
-        }
-
-        return array_list;
-    }
+    // /// c_array must have c_array_len valid elements
+    // fn cArrayToArrayList(gpa: std.mem.Allocator, comptime CType: type, comptime ZType: type, c_array: [*c]const CType, c_array_len: u32) !std.ArrayList(ZType) {
+    //     var array_list = try std.ArrayList(ZType).initCapacity(gpa, c_array_len);
+    //     array_list.items.len = c_array_len;
+    //
+    //     var i: u32 = 0;
+    //     while (i < c_array_len) : (i += 1) {
+    //         array_list.items[i] = @ptrCast(c_array[i]);
+    //     }
+    //
+    //     return array_list;
+    // }
 
     /// used for finding bits that are true within packed structs
     /// ex: vk.DebugUtilsMessageTypeFlagsEXT has many bools, so this function is for finding the one that is true
@@ -96,18 +103,6 @@ pub const Vulkan = struct {
         return null;
     }
 
-    /// caller owns the returned memory
-    fn enumerateVulkanThing(self: *@This(), comptime T: type, comptime vk_enumerate_fn: anytype, extra_args: anytype) !std.ArrayList(T) {
-        var count: u32 = 0;
-        // discarding fine because vulkan-zig converts vk results into zig errors
-        _ = try @call(.auto, vk_enumerate_fn, extra_args ++ .{ &count, null });
-        var array_list = try std.ArrayList(T).initCapacity(self.allocator, count);
-        array_list.items.len = count;
-        _ = try @call(.auto, vk_enumerate_fn, extra_args ++ .{ &count, array_list.items.ptr });
-
-        return array_list;
-    }
-
     fn createInstance(self: *@This(), base_wrapper: vk.BaseWrapper, debug: bool, app_name: [:0]const u8) !void {
         const app_info = vk.ApplicationInfo{
             .api_version = api_version.toU32(),
@@ -117,63 +112,62 @@ pub const Vulkan = struct {
             .p_engine_name = "Djungle",
         };
 
-        var layer_props = try self.enumerateVulkanThing(
-            vk.LayerProperties,
-            vk.BaseWrapper.enumerateInstanceLayerProperties,
-            .{base_wrapper},
-        );
-        defer layer_props.deinit(self.allocator);
+        const layer_props = try base_wrapper.enumerateInstanceLayerPropertiesAlloc(self.allocator);
+        defer self.allocator.free(layer_props);
 
-        var required_layers = std.ArrayList([*:0]const u8).empty;
+        var required_layers = std.ArrayList([:0]const u8).empty;
         defer required_layers.deinit(self.allocator);
         if (debug) try required_layers.append(self.allocator, "VK_LAYER_KHRONOS_validation");
 
-        if (!try allSupported(required_layers.items, vk.LayerProperties, layer_props.items)) {
+        if (!try allSupported(required_layers.items, vk.LayerProperties, layer_props)) {
             return Error.LayerNotSupported;
         }
 
         log.debug(@src(), "Found all required layers", .{});
 
-        var sdl_ext_count: u32 = 0;
-        const sdl_extensions = try sdlCheck(
+        var sdl_ext_count: u32 = undefined;
+        const sdl_extensions: []const [*c]const u8 = (try sdlCheck(
             @src(),
-            [*c]const [*c]const u8,
+            [*]const [*c]const u8,
             c.SDL_Vulkan_GetInstanceExtensions(&sdl_ext_count),
             Error.SdlGetVulkanInstanceExtensionsFailed,
+        ))[0..sdl_ext_count];
+
+        var required_instance_extensions = try self.allocator.alloc(
+            [:0]const u8,
+            if (debug) sdl_extensions.len + 1 else sdl_extensions.len,
         );
+        defer self.allocator.free(required_instance_extensions);
 
-        var required_extensions = try cArrayToArrayList(
-            self.allocator,
-            [*c]const u8,
-            [*:0]const u8,
-            sdl_extensions,
-            sdl_ext_count,
-        );
-        defer required_extensions.deinit(self.allocator);
+        for (sdl_extensions, 0..) |c_ext_name, i|
+            required_instance_extensions[i] = std.mem.span(c_ext_name);
 
-        if (debug) {
-            try required_extensions.append(self.allocator, vk.extensions.ext_debug_utils.name);
-        }
+        if (debug)
+            required_instance_extensions[required_instance_extensions.len - 1] = vk.extensions.ext_debug_utils.name;
 
-        var ext_props = try self.enumerateVulkanThing(
-            vk.ExtensionProperties,
-            vk.BaseWrapper.enumerateInstanceExtensionProperties,
-            .{ base_wrapper, null },
-        );
-        defer ext_props.deinit(self.allocator);
+        const ext_props = try base_wrapper.enumerateInstanceExtensionPropertiesAlloc(null, self.allocator);
+        defer self.allocator.free(ext_props);
 
-        if (!try allSupported(required_extensions.items, vk.ExtensionProperties, ext_props.items)) {
+        if (!try allSupported(required_instance_extensions, vk.ExtensionProperties, ext_props)) {
             return Error.ExtensionNotSupported;
         }
 
         log.debug(@src(), "Found all required extensions", .{});
 
+        const layer_name_ptrs = try self.allocator.alloc([*:0]const u8, required_layers.items.len);
+        defer self.allocator.free(layer_name_ptrs);
+        for (required_layers.items, 0..) |name, i| layer_name_ptrs[i] = name.ptr;
+
+        const ext_name_ptrs = try self.allocator.alloc([*:0]const u8, required_instance_extensions.len);
+        defer self.allocator.free(ext_name_ptrs);
+        for (required_instance_extensions, 0..) |name, i| ext_name_ptrs[i] = name.ptr;
+
         const instance_info = vk.InstanceCreateInfo{
             .p_application_info = &app_info,
-            .enabled_layer_count = @intCast(required_layers.items.len),
-            .pp_enabled_layer_names = required_layers.items.ptr,
-            .enabled_extension_count = @intCast(required_extensions.items.len),
-            .pp_enabled_extension_names = required_extensions.items.ptr,
+            .enabled_layer_count = @intCast(layer_name_ptrs.len),
+            .pp_enabled_layer_names = layer_name_ptrs.ptr,
+            .enabled_extension_count = @intCast(ext_name_ptrs.len),
+            .pp_enabled_extension_names = ext_name_ptrs.ptr,
         };
 
         const instance_handle = try base_wrapper.createInstance(&instance_info, null);
@@ -238,9 +232,73 @@ pub const Vulkan = struct {
         try self.deletion_queue.push(self.allocator, Instance.destroySurfaceKHR, .{ self.instance, self.surface, null });
     }
 
-    // fn choosePhysicalDevice(self: *@This()) !void {
-    //     self.instance.enumeratePhysicalDevices()
-    // }
+    fn isPhysicalDeviceSuitable(self: *@This(), physical_device: *const vk.PhysicalDevice) !bool {
+        const supports_targeted_api_version: bool =
+            self.instance.getPhysicalDeviceProperties(physical_device.*).api_version >= api_version.toU32();
+
+        const queue_families_props = try self.instance.getPhysicalDeviceQueueFamilyPropertiesAlloc(
+            physical_device.*,
+            self.allocator,
+        );
+        defer self.allocator.free(queue_families_props);
+
+        var supports_graphics: bool = false;
+        for (queue_families_props) |queue_family_props| {
+            if (queue_family_props.queue_flags.contains(.{ .graphics_bit = true })) {
+                supports_graphics = true;
+                break;
+            }
+        }
+
+        const device_extensions_props = try self.instance.enumerateDeviceExtensionPropertiesAlloc(
+            physical_device.*,
+            null,
+            self.allocator,
+        );
+        defer self.allocator.free(device_extensions_props);
+
+        const supports_required_extensions = try allSupported(
+            &required_extensions,
+            vk.ExtensionProperties,
+            device_extensions_props,
+        );
+
+        var vulkan11features = vk.PhysicalDeviceVulkan11Features{};
+        var vulkan13features = vk.PhysicalDeviceVulkan13Features{
+            .p_next = &vulkan11features,
+        };
+        var extended_dynamic_state_features_ext = vk.PhysicalDeviceExtendedDynamicStateFeaturesEXT{
+            .p_next = &vulkan13features,
+        };
+        var features2 = vk.PhysicalDeviceFeatures2{
+            .features = .{},
+            .p_next = &extended_dynamic_state_features_ext,
+        };
+
+        self.instance.getPhysicalDeviceFeatures2(physical_device.*, &features2);
+
+        const supports_required_features =
+            vulkan11features.shader_draw_parameters == .true and
+            vulkan13features.synchronization_2 == .true and
+            vulkan13features.dynamic_rendering == .true and
+            extended_dynamic_state_features_ext.extended_dynamic_state == .true;
+
+        return supports_targeted_api_version and supports_graphics and supports_required_extensions and supports_required_features;
+    }
+
+    fn choosePhysicalDevice(self: *@This()) !void {
+        const physical_devices = try self.instance.enumeratePhysicalDevicesAlloc(self.allocator);
+        defer self.allocator.free(physical_devices);
+
+        for (physical_devices) |physical_device| {
+            if (try self.isPhysicalDeviceSuitable(&physical_device)) {
+                self.physical_device = physical_device;
+                return;
+            }
+        }
+
+        return Error.FailedToFindSupportedGPU;
+    }
 
     pub fn init(self: *@This(), gpa: std.mem.Allocator, debug_mode: bool, window: *win.Window, app_name: [:0]const u8) !void {
         self.allocator = gpa;
@@ -264,6 +322,7 @@ pub const Vulkan = struct {
         if (debug_mode) try self.createDebugMessenger();
 
         try self.createSurface();
+        try self.choosePhysicalDevice();
     }
 
     pub fn deinit(self: *@This()) void {
